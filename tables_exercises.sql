54ebd3bbde4b96123c8b7755f66ce22e85ed5670
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-01-06 15:08:27 -0600

    Completed tables exercises for SQL module.

---
+++ tables_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+SHOW TABLES;
+DESCRIBE employees;
+DESCRIBE current_dept_emp;
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_emp_latest_date;
+DESCRIBE dept_manager;
+DESCRIBE salaries;
+DESCRIBE titles;
+SHOW CREATE TABLE dept_manager;
