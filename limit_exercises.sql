bb53fa9fc3487f6bd6391195879988ff45ce1d00
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-01-07 11:49:09 -0600

    Completed limit exercises for SQL module.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -19,18 +19,20 @@ ORDER BY hire_date LIMIT 5;
 # Find employees hired in the 90s and born on Christmas
 # sorted by hire date and limited to just 5 results
 # and offset to provide the tenth page of results given
-# that each page has 5 results.
+# that each page has 5 results. Offset is 45 because the
+# tenth page will be at offset 10 * 5 - 5 since the offset
+# for the first page is 0.
 # Names returned:
-#	Christophe Baca
-#	Moie Birsak
-#	Chikako Ibel
-#	Shounak Jansen
-#	Zhigen Boissier
+#	Pranay Narwekar
+#	Marjo Farrow
+#	Ennio Karcich
+#	Dines Lubachevsky
+#	Ipke Fontan
 SELECT * FROM employees 
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' 
 	AND DATE_FORMAT(birth_date, '%m-%d') = DATE_FORMAT('9999-12-25', '%m-%d')
-ORDER BY hire_date LIMIT 5 OFFSET 50;
+ORDER BY hire_date LIMIT 5 OFFSET 45;
 
 # 4.
 # LIMIT will specify the number of rows to show on a page and OFFSET will specify the page number in the form
-# (page number * number of rows per page).
+# (page number * number of rows per page - number of rows per page).
