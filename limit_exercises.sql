785725154731920419d3a2b82c62b08a4f9b4075
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-01-07 13:30:55 -0600

    Completed limit exercises for SQL module.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,8 +1,10 @@
 USE employees;
 
+# 2
 # List the first 10 distinct last names in employees sorted in descending order
 SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 
+# 3
 # Find employees hired in the 90s and born on Christmas
 # sorted by hire date and limited to just the first 5 results
 # Names returned:
@@ -16,6 +18,7 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 	AND DATE_FORMAT(birth_date, '%m-%d') = DATE_FORMAT('9999-12-25', '%m-%d')
 ORDER BY hire_date LIMIT 5;
 
+# 4
 # Find employees hired in the 90s and born on Christmas
 # sorted by hire date and limited to just 5 results
 # and offset to provide the tenth page of results given
@@ -33,6 +36,5 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 	AND DATE_FORMAT(birth_date, '%m-%d') = DATE_FORMAT('9999-12-25', '%m-%d')
 ORDER BY hire_date LIMIT 5 OFFSET 45;
 
-# 4.
 # LIMIT will specify the number of rows to show on a page and OFFSET will specify the page number in the form
 # (page number * number of rows per page - number of rows per page).
