ec48f5bf4e06582d12aae0d37a0c45e90ad6b28b
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-01-11 14:42:08 -0600

    Completed subqueries exercises for SQL module.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,14 +1,13 @@
 # 1
 # Find all the current employees with the same hire date as employee 101010 using a sub-query.
 SELECT
-	employees.emp_no,
-	employees.hire_date
+	emp_no,
+	hire_date
 FROM employees
-JOIN dept_emp
-    ON employees.emp_no = dept_emp.emp_no
-WHERE employees.hire_date = 
+WHERE hire_date = 
 	(SELECT hire_date FROM employees WHERE emp_no = 101010)
-    AND dept_emp.to_date > CURDATE();
+    AND emp_no IN 
+		(SELECT emp_no FROM dept_emp WHERE to_date > CURDATE());
 
 # 2
 # Find all the titles ever held by all current employees with the first name Aamod.
@@ -48,34 +47,46 @@ WHERE emp_no IN
 # 5
 # Find all the employees who currently have a higher salary than the company's overall, historical average salary.
 SELECT
-	CONCAT(employees.first_name, ' ', employees.last_name),
-	salaries.salary
-FROM employees
-JOIN salaries
-	ON employees.emp_no = salaries.emp_no
-WHERE salaries.to_date > CURDATE()
-	AND salaries.salary > (SELECT AVG(salary) FROM salaries);
+	emp_no,
+	salary
+FROM salaries
+WHERE to_date > CURDATE()
+	AND salary > (SELECT AVG(salary) FROM salaries);
 
 # 6
 # How many current salaries are within 1 standard deviation of the current highest salary? 
+# 211
 # (Hint: you can use a built in function to calculate the standard deviation.) 
-# What percentage of all salaries is this?
 SELECT COUNT(*)
 FROM salaries
 WHERE (SELECT STDDEV(salary) FROM salaries) >
 	(SELECT MAX(salary) FROM salaries) - salary;
 
+# What percentage of all salaries is this?
+# 0.0074%
+SELECT 
+(
+	SELECT COUNT(*)
+	FROM salaries
+	WHERE (SELECT STDDEV(salary) FROM salaries) >
+		(SELECT MAX(salary) FROM salaries) - salary
+)
+/ COUNT(*) * 100 FROM salaries;
+
 # BONUS
 
 # 1
 # Find all the department names that currently have female managers.
-SELECT departments.dept_name
+SELECT dept_name
 FROM departments
-JOIN dept_manager
-	ON departments.dept_no = dept_manager.dept_no
-WHERE dept_manager.to_date > CURDATE()
-	AND dept_manager.emp_no IN
-		(SELECT emp_no FROM employees WHERE gender = 'F');
+WHERE dept_no IN
+(
+	SELECT dept_no
+	FROM dept_manager
+	WHERE emp_no IN
+		(SELECT emp_no FROM employees WHERE gender = 'F')
+		AND to_date > CURDATE()
+);
 
 # 2
 # Find the first and last name of the employee with the highest salary.
