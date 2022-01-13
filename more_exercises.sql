27b588775eefce1bf063bd4a7719acb47ad66682
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-01-13 17:00:06 -0600

    Incomplete more exercises for SQL module.

--- more_exercises.sql
+++ more_exercises.sql
@@ -146,3 +146,167 @@ WHERE city.Name = @x;
 
 # 6
 # What is the life expectancy in city x?
+SET @x = 'San Antonio';
+
+SELECT
+	country.LifeExpectancy,
+	@x As X
+FROM country
+JOIN city
+	ON country.Code = city.CountryCode
+WHERE city.Name = @x;
+
+# Sakila Database
+
+USE Sakila;
+
+# First group of exercises
+
+# 1a
+# Select all columns from the actor table.
+SELECT * FROM actor;
+
+# 1b
+# Select only the last_name column from the actor table.
+SELECT last_name FROM actor;
+
+# 1c
+# Select only the film_id, title, and release_year columns from the film table.
+SELECT film_id, title, release_year FROM film;
+
+# 2a
+# Select all distinct (different) last names from the actor table.
+SELECT DISTINCT last_name FROM actor;
+
+# 2b
+# Select all distinct (different) postal codes from the address table.
+SELECT DISTINCT postal_code FROM address;
+
+# 2c
+# Select all distinct (different) ratings from the film table.
+SELECT DISTINCT rating FROM film;
+
+# 3a
+# Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
+SELECT title, description, rating, length
+FROM film
+WHERE length >= (3 * 60);
+
+# 3b
+# Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date >= '2005-05-27';
+
+# 3c
+# Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date LIKE '2005-05-27%';
+
+# 3d
+# Select all columns from the customer table for rows that have a last names beginning with S and a first names ending with N.
+SELECT * FROM customer 
+WHERE first_name LIKE '%n'
+	AND last_name LIKE 'S%';
+
+# 3e
+# Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M".
+SELECT * FROM customer 
+WHERE NOT active
+	OR last_name LIKE 'M%';
+
+# 3f
+# Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either C, S or T.
+SELECT * FROM category
+WHERE category_id > 4
+	AND name REGEXP '^[C|S|T].*';
+
+# 3g
+# Select all columns minus the password column from the staff table for rows that contain a password.
+# Solution found here:
+# https://stackoverflow.com/questions/2365972/how-can-i-select-from-a-table-in-mysql-but-omit-certain-columns/13808457#13808457
+SET @sql = 
+	CONCAT('SELECT ',
+			(SELECT GROUP_CONCAT(COLUMN_NAME)
+			FROM information_schema.columns
+			WHERE table_schema = 'sakila'
+				AND table_name = 'staff' 
+				AND column_name NOT IN ('password')),
+			' FROM staff WHERE password IS NOT NULL');  
+PREPARE stmt1 FROM @sql;
+EXECUTE stmt1;
+
+# 3h
+# Select all columns minus the password column from the staff table for rows that do not contain a password.
+# I must be overthinking this solution, or are we actually supposed to type out each column name?
+SET @sql = 
+	CONCAT('SELECT ',
+			(SELECT GROUP_CONCAT(COLUMN_NAME)
+			FROM information_schema.columns
+			WHERE table_schema = 'sakila'
+				AND table_name = 'staff' 
+				AND column_name NOT IN ('password')),
+			' FROM staff WHERE password IS NULL');  
+PREPARE stmt1 FROM @sql;
+EXECUTE stmt1;
+
+# 4a
+# Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java.
+SELECT phone, district
+FROM address
+WHERE district IN ('California', 'England', 'Taipei', 'West Java');
+
+# 4b
+# Select the payment id, amount, and payment date columns from the payment table for payments made on 05/25/2005, 05/27/2005, 
+# and 05/29/2005. (Use the IN operator and the DATE function, instead of the AND operator as in previous exercises.)
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE DATE(payment_date) IN ('2005-05-25', '2005-05-27', '2005-05-29');
+
+# 4c
+# Select all columns from the film table for films rated G, PG-13 or NC-17.
+SELECT * FROM film
+WHERE rating IN ('G', 'PG-13', 'NC-17');
+
+# 5a
+# Select all columns from the payment table for payments made between midnight 05/25/2005 and 1 second before midnight 05/26/2005.
+SELECT * FROM payment
+WHERE payment_date BETWEEN '2005-05-25 00:00:00' AND '2005-05-25 23:59:59';
+
+# 5b
+# Select the film_id, title, and description columns from the film table for films where the length of the description is between 100 and 120.
+SELECT film_id, title, description
+FROM film
+WHERE length BETWEEN 100 AND 120;
+
+# 6a
+# Select all columns from the film table for rows where the description begins with "A Thoughtful".
+SELECT * FROM film
+WHERE description LIKE 'A Thoughtful%';
+
+# 6b
+# Select all columns from the film table for rows where the description ends with the word "Boat".
+SELECT * FROM film
+WHERE description LIKE '%Boat';
+
+# 6c
+# Select all columns from the film table where the description contains the word "Database" and the length of the film is greater than 3 hours.
+SELECT * FROM film
+WHERE description LIKE '%Database%'
+	AND length > (3 * 60);
+
+# 7a
+# Select all columns from the payment table and only include the first 20 rows.
+SELECT * FROM payment LIMIT 20;
+
+# 7b
+# Select the payment date and amount columns from the payment table for rows where the payment amount 
+# is greater than 5, and only select rows whose zero-based index in the result set is between 1000-2000.
+SELECT payment_date, amount
+FROM payment
+WHERE amount > 5
+LIMIT 1001 OFFSET 999;
+
+# 7c
+# Select all columns from the customer table, limiting results to those where the zero-based index is between 101-200.
